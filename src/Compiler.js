b891e1e14203073ab5a83a00dbfb869f08464dac
ynpan <ynpan@thoughtworks.com>
2020-01-28 14:32:13 +0800

    add Compiler class

---
+++ src/Compiler.js
@@ -0,0 +1,13 @@
+export class Compiler {
+    constructor(options) {
+        const { entry, output } = options;
+        this.entry = entry;
+        this.output = output;
+        this.module = []
+    }
+
+    run(){}
+
+    generate(){}
+
+}
